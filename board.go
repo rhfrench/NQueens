e1ffba3fe4f8af901784a9dc9e22194417182cd8
RFRENCHLAPTOP\ryanf <ryanfrench@ymail.com>
2018-09-16 16:02:44 -0700

    board setup

--- board.go
+++ board.go
@@ -1,10 +1,24 @@
 package N_Queens
 
+import "fmt"
+
 const N int = 4
 
+type Point struct {
+	x int
+	y int
+}
 
-func createBoard() [N][N] int {
-	var board[N][N] int
+func CreateBoard() [][] bool {
+	fmt.Println("Initializing Chess Board..")
+	var board = make([][]bool, N)       // initialize a slice of dy slices
+	for i:=0;i<N;i++ {
+		board[i] = make([]bool, N)  // initialize a slice of dx unit8 in each of dy slices
+	}
 
 	return board
+}
+
+func PlaceQueen() {
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package N_Queens
 
+import (
+	"fmt"
+	"time"
+)
+
+var startTime time.Time
+var numOfScenarios int = 0
+var numOfSeconds float64 = 0
+var numOfSolutions int64 = 0
+
 func main() {
 
+var board[][]bool = CreateBoard()
+  var i int = 0
+
+	for i = 0; i <= N; i++ {
+		startTime := time.Now()
+		board[0][1] = true
 
 
+
+
+
+		duration := time.Since(startTime)
+		numOfSeconds = duration.Seconds()
+
+		fmt.Println("Solutions:  ", numOfSolutions, "-- exec time:", numOfSeconds)
+	}
 }
